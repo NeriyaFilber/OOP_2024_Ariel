b794345246ebefcc7fb41c3e1aaf1760c35e5066
Moshe Ofer <mosheofer1@gmail.com>
2024-11-11 18:04:23 +0200

    t2

---
+++ TA 2/src/out_main.java
@@ -0,0 +1,10 @@
+public class out_main {
+    // Main method to test the blocks
+    public static void main(String[] args) {
+        System.out.println("Creating first instance:");
+        InitializationBlockDemo demo1 = new InitializationBlockDemo();
+
+        System.out.println("\nCreating second instance:");
+        InitializationBlockDemo demo2 = new InitializationBlockDemo();
+    }
+}
